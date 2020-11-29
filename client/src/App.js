1a1720e3b7cf63474b18c15d53ad676c0bc4cf8e
jimin <seojiminca@gmail.com>
2020-11-29 21:37:03 +0900

    Revise client App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <div>
-                <h1>Register</h1>
+                <Link to={'/register'}>
+                    <span>Register</span>
+                </Link>
+                <Link to={'/signin'}>
+                    <span>Sign in</span>
+                </Link>
             </div>
         </div>
     );
