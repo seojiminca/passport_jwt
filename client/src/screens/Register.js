4d5eadc7dca2c696fbd0d10599b5ff6ce57b926e
jimin <seojiminca@gmail.com>
2020-12-01 23:28:22 +0900

    Add register.js

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,10 +1,19 @@
 //rsc
-import React from 'react';
+import React, {useState} from 'react';
 
 const Register = () => {
-    return (
-        <div>
+    const [formData, setFormData] = useState({
+        email: '',
+        name: '',
+        password: '',
+        textChange: 'Sign in'
+    });
 
+    return (
+        <div className="App">
+            <div>
+                <h1>Sign in</h1>
+            </div>
         </div>
     );
 };
