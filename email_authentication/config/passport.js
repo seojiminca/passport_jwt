891c1be5226b79b411c93a97ae8025eebc3ae13a
jimin <seojiminca@gmail.com>
2020-12-27 23:55:15 +0900

    Add passport-config

--- email_authentication/config/passport.js
+++ email_authentication/config/passport.js
@@ -1,5 +1,10 @@
 const LocalStrategy = require('passport-local').Strategy;
 
 function initialize(passport) {
-    
+    const authenticateUser = (email, hashed, done) => {
+
+    }
+    passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'hashed'}), authenticateUser)
+    passport.serializeUser((user, done) => {})
+    passport.deserializeUser((id, done) => {})
 }
