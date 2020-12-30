4398885a47a2ddb0a1a54b9f57f6c1c3a269558d
jimin <seojiminca@gmail.com>
2020-12-30 23:40:13 +0900

    fixed errors

--- email_authentication/server.js
+++ email_authentication/server.js
@@ -29,9 +29,9 @@ initializePassport(passport);
 
 //required for passport
 app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: true,
-    saveUninitialized: true
+    secret: process.env.SECRET_KEY, //session을 암호화해줌.
+    resave: false, //Should you resave if nothing has changed.
+    saveUninitialized: false //Do you want to save empty value in the session if there is no value.
 }));
 app.use(cookieParser(process.env.SECRET_KEY)) //same secret key with session.
 
