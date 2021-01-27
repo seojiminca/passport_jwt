a84bbf609208b1f759046ff035d6d56a3b1da617
jimin <seojiminca@gmail.com>
2021-01-27 23:31:03 +0900

    Add error-handler

--- email_authentication/middleware/error-handler.js
+++ email_authentication/middleware/error-handler.js
@@ -1,4 +1,8 @@
 
 module.exports = function errorHandler(err, req, res, next) {
+    if(typeof (err) === 'string'){
+        return res.status(400).json({ message: err });
+    }
 
+    return res.status(500).json({ message: err.message });
 }
