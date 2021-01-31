43d817df2670f7c6fbc7a101fa21aab4c4dfd15a
jimin <seojiminca@gmail.com>
2021-01-31 22:38:03 +0900

    Update error-handler

--- email_authentication/middleware/error-handler.js
+++ email_authentication/middleware/error-handler.js
@@ -1,7 +1,17 @@
 
 module.exports = function errorHandler(err, req, res, next) {
+    // custom application error
     if(typeof (err) === 'string'){
         return res.status(400).json({ message: err });
+    }
+
+    if (err.name === 'ValidationError') {
+        // mongoose validation error
+        return res.status(400).json({ message: err.message });
+    }
+
+    if (err.name === 'UnauthorizedError') {
+        return res.status(401).json({ message: 'Invalid Token' });
     }
 
     return res.status(500).json({ message: err.message });
