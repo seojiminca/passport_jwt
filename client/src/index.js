b71c321001adef3cefd5403b3ced9f591ec48442
jimin <seojiminca@gmail.com>
2021-02-11 19:29:48 +0900

    fixed client err

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Register from 'screens/Register';
+import Register from './screens/Register';
 import Signin from "./screens/Signin";
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,4 +1,4 @@
-//const {Strategy, ExtractJwt} = require('passport-jwt'); //여기서는 JwtStrategy 사용못함. type error!
+//const {Strategy, ExtractJwt} = require('server'); //여기서는 JwtStrategy 사용못함. type error!
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const userModel = require('../user/user_model');
